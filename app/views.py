b7d9ff94929b27a0db38c0e982cb0cb655bcd1fd
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2021-09-01 01:15:54 -0500

    add dependecies

--- app/views.py
+++ app/views.py
@@ -4,15 +4,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
 
 from accounts.models import User, Profile
 from commons.models import Specialty
-from core import settings
-
 
 @login_required(login_url="/login/")
 def index(request):
