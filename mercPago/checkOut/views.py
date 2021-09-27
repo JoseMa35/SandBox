6080a9de811261ce7be7e681c7c541f69b0f75d3
JoseManuel <josemcastilloc@gmail.com>
2021-09-27 14:45:23 -0300

    refundProposal

--- mercPago/checkOut/views.py
+++ mercPago/checkOut/views.py
@@ -23,7 +23,6 @@ import mercadopago
 #sdk = mercadopago.SDK("TEST-6367338741216916-081714-571bcf58c036d48cd8265c82a4e3a894-417944042")
 
 
-
 class MercadoPagoApiView(APIView):
 # Crea un ítem en la preferencia
     #SDK_MERCADO = mercadopago.SDK("TEST-2734577806631487-052504-21c1748fdb47e0fd69b3399272c9f3dc-465550336") 
@@ -50,64 +49,37 @@ class MercadoPagoApiView(APIView):
         preference_response = self.SDK_MERCADO_Jose.preference().create(preference_data)
         preference = preference_response["response"]
         print("data preference", preference)
+      
         return Response(
             {
                 "sandbox_init_point" : preference["sandbox_init_point"],
-                "init_point": preference["init_point"]
+                "init_point": preference["init_point"],
+                "payment_id": preference["id"]
             } # paso 4 devolvermos las url obtenedias en la creacion de preferencia
             )
 
 
-# payment_data = {
-#     "status": "cancelled"
+
+class Refund(APIView):
+
+    SDK_MERCADO_Jose = mercadopago.SDK("TEST-2734577806631487-052504-21c1748fdb47e0fd69b3399272c9f3dc-465550336")  
+
+    def post(self, request):
+        print(" datos a recibir ", request.data)
+        payment_data  = {
+            "status": "canceled"   
+        }
+        payment_id= MercadoPagoApiView.payment_id
+        payment_response = self.SDK_MERCADO_Jose.payment().update(payment_id, payment_data)
+        payment = payment_response["response"]
+        refund_response = self.SDK_MERCADO_Jose.refund().create(payment_id)
+        refund = refund_response["response"]
+
+    def listAll(self, request):
+        
+        refunds_response = self.SDK_MERCADO_Jose.refund().list_all(payment_id)
+        refunds = refunds_response["response"]
     
-# }
-
-# payment_id = uuid.uuid4().hex #debe generar un Payment ID en Hexa de 32 caracteres
-# print(payment_id)
-# payment_response = sdk.payment().update(payment_id, payment_data)
-# payment = payment_response["response"]
-
-## #refound
-## refund_response = sdk.refund().create(payment_id)
-## refund = refund_response["response"]
-#
-#class ListadoItems(APIView):
-#    #renderer_classes = [TemplateHTMLRenderer]
-#    #template_name = 'Item_detail.html'
-#
-#
-#    def get(self, request):
-#        # profile = get_object_or_404(Item)
-#        # serializer= ItemSerializer(profile)
-#        items = Item.objects.all()
-#        return render(request,'checkOut.html',{'items':items,'preference_id':preference['id']})  
-#
-#    def post(self, request):
-#        profile = get_object_or_404(Item)
-#        serializer= ItemSerializer(Item)    
-#        if not serializer.is_valid():
-#            return Response({'serializer':serializer, 'Item':Item})  
-#        serializer.save()
-#        return redirect('articulos')    
-#
-#class SuccesfulPayView(APIView):
-#
-#    def get(self, request):
-#        return render(request,'succesfulPay.html',{}) 
-#
-#class FailPayView(APIView):
-#
-#    def get(self, request):
-#        return render(request,'failurePay.html',{}) 
-#
-#class PendingPayView(APIView):
-#
-#    def get(self, request):
-#        return render(request,'pendingPay.html',{})         
-#
-
-
 
 
       
