167b38d44ec45fc79b239c71a8eb8063d87373a0
JoseManuel <josemcastilloc@gmail.com>
2021-09-28 22:28:02 -0300

    CRUD_a revisar

--- mercPago/checkOut/views.py
+++ mercPago/checkOut/views.py
@@ -6,7 +6,7 @@ from django.http.response import JsonResponse
 from django.core import serializers
 from django.http import HttpResponse
 from checkOut.models import Item
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer,StaticHTMLRenderer
@@ -21,7 +21,6 @@ import mercadopago
 
     # Agrega credenciales
 #sdk = mercadopago.SDK("TEST-6367338741216916-081714-571bcf58c036d48cd8265c82a4e3a894-417944042")
-
 
 class MercadoPagoApiView(APIView):
 # Crea un ítem en la preferencia
@@ -59,27 +58,45 @@ class MercadoPagoApiView(APIView):
             )
 
 
-
 class Refund(APIView):
 
     SDK_MERCADO_Jose = mercadopago.SDK("TEST-2734577806631487-052504-21c1748fdb47e0fd69b3399272c9f3dc-465550336")  
-
-    def post(self, request):
+    
+    def update(self, request):
         print(" datos a recibir ", request.data)
         payment_data  = {
-            "status": "canceled"   
+            "status": "canceled" 
         }
-        payment_id= MercadoPagoApiView.payment_id
+        
+        payment_id = self.request.data['id']
         payment_response = self.SDK_MERCADO_Jose.payment().update(payment_id, payment_data)
         payment = payment_response["response"]
-        refund_response = self.SDK_MERCADO_Jose.refund().create(payment_id)
+        print("Payment data", payment)
+        return Response({
+            "status": "Ok"
+        },
+        status = status.HTTP_200_OK
+        )  
+
+    def post(self, request):
+        #print(" datos a recibir ", request.data)
+        pay_id = self.request.data['id']
+
+        refund_response = self.SDK_MERCADO_Jose.refund().create(payment_id=pay_id)
         refund = refund_response["response"]
+        print("Refund data", refund)
+        return Response({
+            "status": "Ok"
+        },
+        status = status.HTTP_200_OK
+        )          
 
-    def listAll(self, request):
-        
-        refunds_response = self.SDK_MERCADO_Jose.refund().list_all(payment_id)
-        refunds = refunds_response["response"]
-    
-
+    def get(self, request):
+        #payment_id = request.data
+        pay_id = self.request.data['id']
+        refunds_list = self.SDK_MERCADO_Jose.refund().list_all(payment_id=pay_id)
+        refunds = refunds_list["response"]
+        print("data preference", refunds)
+        return Response(refunds)
 
       
